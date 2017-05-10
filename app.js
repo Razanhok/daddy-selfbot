b5e564236e4deda922cc9cde40791d0280a2a6e3
Damian Blume <damian.blume@gmail.com>
2017-05-10 20:36:44 +0200

    Initial setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const komada = require('komada')
+
+komada.start({
+  botToken: process.env.BOT_TOKEN,
+  ownerID: process.env.OWNER_ID,
+  clientID: process.env.CLIENT_ID,
+  prefix: '//',
+  clientOptions: {
+    fetchAllMembers: true
+  }
+})
